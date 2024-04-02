43f385fa06b3ab78d74424cd6e550550eb41f4e8
Martipolo <martipolo@gmail.com>
2024-04-02 11:38:37 +0200

    Modif login.php

--- login.php
+++ login.php
@@ -6,6 +6,9 @@
         if($login=="Peter" && $pwd=="Parker"){
                 $_SESSION['login']=$login;
                 echo "Bienvenue " . $login;
+                ?>
+                <a href="index.php">Accéder à GOD!!</a>
+                <?php
         }else{
                 echo "Echec";
         }
