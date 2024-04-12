65bf2439ef469d21fbdc94e6695af22b885f81bd
Martipolo <martipolo@gmail.com>
2024-04-12 14:15:41 +0200

    Ajout auth avec table sql

--- login.php
+++ login.php
@@ -1,11 +1,31 @@
 <?php
+
+// Connexion à la base de données
+$serveur = "10.5.40.46:3306";
+$utilisateur = "polo";
+$motdepasse = "polo";
+$basededonnees = "localisationdrone";
+
+$connexion = new PDO("mysql:host=$serveur;dbname=$basededonnees", $utilisateur, $motdepasse, );
+
+// Vérifier la connexion
+if ($connexion->connect_error) {
+    die("Connexion échouée : " . $connexion->connect_error);
+}
+
+
         session_start();
         $login = $_POST['login'];
         $pwd = $_POST['password'];
 
-        if($login=="Peter" && $pwd=="Parker"){
-                $_SESSION['login']=$login;
-                echo "Bienvenue " . $login;
+// Requête SQL pour vérifier l'existence de l'utilisateur
+$sql = "SELECT * FROM authentification WHERE nom = '$login' AND password = '$password'";
+$resultat = $connexion->query($sql);
+
+
+if ($resultat->num_rows > 0) {
+    // L'utilisateur est authentifié
+    echo "Authentification réussie ! Bienvenue, $login.";
                 ?>
                 <br><br><a href="index.php">Accéder à GOD!!</a>
                 <?php
